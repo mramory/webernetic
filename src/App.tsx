b77ac187f4b5503632df4fdfbcabfc86a56b9243
mramory <mramor91932765@gmail.com>
2024-06-11 15:49:27 +0300

    modal fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import s from "./scss/pages/Page.module.scss"
 import image from "./assets/image.png"
 import { Modal } from "./components/Modal"
 import { useState } from "react"
+import { Input } from "./components/ui/Input"
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
@@ -23,7 +24,24 @@ function App() {
       <div className={s.image}>
         <img src={image} />
       </div>
-      <Modal isOpen={isOpen} handleClose={() => setIsOpen(false)} />
+      <Modal isOpen={isOpen} handleClose={() => setIsOpen(false)}>
+        <div className={s.modal}>
+          <h2>Войти в систему</h2>
+          <div className={s.inputs}>
+            <Input placeholder="Email/Телефон" />
+            <Input placeholder="Пароль" />
+          </div>
+          <div className={s.checkbox}>
+            <input type="checkbox" id="remind" name="remind" />
+            <label htmlFor="remind">Запомнить пароль</label>
+          </div>
+          <a>Восстановить</a>
+          <div className={s.btns}>
+            <Button>Войти</Button>
+            <Button variant="secondary">Зарегестрироваться</Button>
+          </div>
+        </div>
+      </Modal>
     </main>
   )
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,36 +2,21 @@ import ReactPortal from "./utils/ReactPortal"
 import s from "../scss/components/Modal.module.scss"
 import clsx from "clsx"
 import x from "../assets/x.svg"
-import { Input } from "./ui/Input"
-import { Button } from "./ui/Button"
+
 
 interface ModalProps {
     isOpen: boolean
     handleClose: () => void
+    children?: React.ReactNode
 }
 
-export const Modal = ({ isOpen, handleClose }: ModalProps) => {
+export const Modal = ({ isOpen, handleClose, children }: ModalProps) => {
     return (
         <ReactPortal wrapperId="modal-wrapper">
             <div className={clsx(s.modal, isOpen ? s.open : s.close)}>
                 <div className={s.modal_content}>
                     <div className={s.close_btn_container}><button className={s.close_btn} onClick={() => handleClose()}><img src={x} /></button></div>
-                    <div>
-                        <h2>Войти в систему</h2>
-                        <div className={s.inputs}>
-                            <Input placeholder="Email/Телефон" />
-                            <Input placeholder="Пароль" />
-                        </div>
-                        <div className={s.checkbox}>
-                            <input type="checkbox" id="remind" name="remind" />
-                            <label htmlFor="remind">Запомнить пароль</label>
-                        </div>
-                        <a>Восстановить</a>
-                        <div className={s.btns}>
-                            <Button>Войти</Button>
-                            <Button variant="secondary">Зарегестрироваться</Button>
-                        </div>
-                    </div>
+                    {children}
                 </div>
             </div>
         </ReactPortal>
